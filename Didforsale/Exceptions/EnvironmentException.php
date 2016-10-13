909753e88dd76e267c058f18568899e0e0a5efd3
Raakesh Kumar <kumar3180@gmail.com>
2016-10-13 19:44:01 +0530

    Started working on SMS API

---
+++ Didforsale/Exceptions/EnvironmentException.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Didforsale\Exceptions;
+
+/**
+ * Description of EnvironmentException
+ *
+ * @author raakesh
+ */
+class EnvironmentException extends DidforsaleException {
+    //put your code here
+}
